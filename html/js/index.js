cc1d81c672382cb3203c3e2d96630f630e603c4f
indalecio <jfgarciagomez@gmail.com>
2017-10-10 19:13:09 +0200

    añadidos actividades sesion 5

--- html/js/index.js
+++ html/js/index.js
@@ -1,14 +1,7 @@
 function mostrarN(dir){
     a = dir;
     a = a.substring(1);
-    dat = a.split("&");
-    nom = dat[0].split("=");
-    fto = dat[1].split("=");
-    document.getElementById("nombr").innerHTML = "Hola " + nom[1]+ ": Bienvenido a smartroom.";
-    salida = "Hola " + nom[1]+ ": Bienvenido a smartroom.";
-    return  salida;
-}
-function aumentar(imag) {
+    dat = a.split("&");function aumentar(imag) {
     imag.style.height = "120px";
     imag.style.width = "px";
 }
@@ -16,6 +9,13 @@ function aumentar(imag) {
 function normal(imag) {
     imag.style.height = "80px";
     imag.style.width = "px";
+}
+
+    nom = dat[0].split("=");
+    fto = dat[1].split("=");
+    document.getElementById("nombr").innerHTML = "Hola " + nom[1]+ ": Bienvenido a smartroom.";
+    salida = "Hola " + nom[1]+ ": Bienvenido a smartroom.";
+    return  salida;
 }
 
 function diferencia (max, min){
