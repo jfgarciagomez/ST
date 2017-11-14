31a1b957accba8a711950432c74690c1531a4ef0
indalecio <jfgarciagomez@gmail.com>
2017-11-14 10:36:45 +0100

    xml para mostrar opciones de ciudades segun el pais elegido tablas hacemos un array de un xml y lo usamos en la tabla solo funciona en depurador

--- html/js/config.js
+++ html/js/config.js
@@ -1,24 +1,3 @@
-function mostrarN(dir){
-    a = dir;
-    a = a.substring(1);
-    dat = a.split("&");
-    nom = dat[0].split("=");
-    fto = dat[1].split("=");
-    document.getElementById("nombr").innerHTML = "Hola " + nom[1]+ ": Bienvenido a smartroom.";
-    salida = "Hola " + nom[1]+ ": Bienvenido a smartroom.";
-    return  salida;
-}
-
-function aumentar(imag) {
-    imag.style.height = "120px";
-    imag.style.width = "px";
-}
-
-function normal(imag) {
-    imag.style.height = "80px";
-    imag.style.width = "px";
-}
-
 
 function diferencia (tmax, tmin){
     //max.style.color = "blue";
@@ -131,7 +110,7 @@ function adaptecoor(coor){
 
 function checkpais(pais){
     var p=pais.value;
-    p=p.toUpperCase();
+    var p=p.toUpperCase();
     var listap=["AFGHANISTAN", "ALBANIA", "ALGERIA", "AMERICAN SAMOA", "ANDORRA", "ANGOLA", "ANGUILLA", "ANTARCTICA", "ANTIGUA AND BARBUDA", "ARGENTINA", "ARMENIA", "ARUBA", "AUSTRALIA", "AUSTRIA", "AZERBAIJAN", "BAHAMAS", "BAHRAIN", "BANGLADESH", "BARBADOS", "BELARUS", "BELGIUM", "BELIZE", "BENIN", "BERMUDA", "BHUTAN", "BOLIVIA", "BOSNIA AND HERZEGOVINA", "BOTSWANA", "BOUVET ISLAND", "BRAZIL", "BRITISH INDIAN OCEAN TERRITORY", "BRUNEI DARUSSALAM", "BULGARIA", "BURKINA FASO", "BURUNDI", "CAMBODIA", "CAMEROON", "CANADA", "CAPE VERDE", "CAYMAN ISLANDS", "CENTRAL AFRICAN REPUBLIC", "CHAD", "CHILE", "CHINA", "CHRISTMAS ISLAND", "COCOS (KEELING) ISLANDS", "COLOMBIA", "COMOROS", "CONGO", "CONGO, THE DEMOCRATIC REPUBLIC OF THE", "COOK ISLANDS", "COSTA RICA", "COTE D'IVOIRE", "CROATIA", "CUBA", "CYPRUS", "CZECH REPUBLIC", "DENMARK", "DJIBOUTI", "DOMINICA", "DOMINICAN REPUBLIC", "ECUADOR", "EGYPT", "EL SALVADOR", "EQUATORIAL GUINEA", "ERITREA", "ESTONIA", "ETHIOPIA", "FALKLAND ISLANDS (MALVINAS)", "FAROE ISLANDS", "FIJI", "FINLAND", "FRANCE", "FRENCH GUIANA", "FRENCH POLYNESIA", "FRENCH SOUTHERN TERRITORIES", "GABON", "GAMBIA", "GEORGIA", "GERMANY", "GHANA", "GIBRALTAR", "GREECE", "GREENLAND", "GRENADA", "GUADELOUPE", "GUAM", "GUATEMALA", "GUINEA", "GUINEA-BISSAU", "GUYANA", "HAITI", "HEARD ISLAND AND MCDONALD ISLANDS", "HOLY SEE (VATICAN CITY STATE)", "HONDURAS", "HONG KONG", "HUNGARY", "ICELAND", "INDIA", "INDONESIA", "IRAN, ISLAMIC REPUBLIC OF", "IRAQ", "IRELAND", "ISRAEL", "ITALY", "JAMAICA", "JAPAN", "JORDAN", "KAZAKHSTAN", "KENYA", "KIRIBATI", "KOREA, DEMOCRATIC PEOPLE'S REPUBLIC OF", "KOREA, REPUBLIC OF", "KUWAIT", "KYRGYZSTAN", "LAO PEOPLE'S DEMOCRATIC REPUBLIC", "LATVIA", "LEBANON", "LESOTHO", "LIBERIA", "LIBYAN ARAB JAMAHIRIYA", "LIECHTENSTEIN", "LITHUANIA", "LUXEMBOURG", "MACAO", "MACEDONIA, THE FORMER YUGOSLAV REPUBLIC OF", "MADAGASCAR", "MALAWI", "MALAYSIA", "MALDIVES", "MALI", "MALTA", "MARSHALL ISLANDS", "MARTINIQUE", "MAURITANIA", "MAURITIUS", "MAYOTTE", "MEXICO", "MICRONESIA, FEDERATED STATES OF", "MOLDOVA, REPUBLIC OF", "MONACO", "MONGOLIA", "MONTENEGRO", "MONTSERRAT", "MOROCCO", "MOZAMBIQUE", "MYANMAR", "NAMIBIA", "NAURU", "NEPAL", "NETHERLANDS", "NETHERLANDS ANTILLES", "NEW CALEDONIA", "NEW ZEALAND", "NICARAGUA", "NIGER", "NIGERIA", "NIUE", "NORFOLK ISLAND", "NORTHERN MARIANA ISLANDS", "NORWAY", "OMAN", "PAKISTAN", "PALAU", "PALESTINIAN TERRITORY, OCCUPIED", "PANAMA", "PAPUA NEW GUINEA", "PARAGUAY", "PERU", "PHILIPPINES", "PITCAIRN", "POLAND", "PORTUGAL", "PUERTO RICO", "QATAR", "REUNION", "ROMANIA", "RUSSIAN FEDERATION", "RWANDA", "SAINT HELENA", "SAINT KITTS AND NEVIS", "SAINT LUCIA", "SAINT PIERRE AND MIQUELON", "SAINT VINCENT AND THE GRENADINES", "SAMOA", "SAN MARINO", "SAO TOME AND PRINCIPE", "SAUDI ARABIA", "SENEGAL", "SERBIA", "SEYCHELLES", "SIERRA LEONE", "SINGAPORE", "SLOVAKIA", "SLOVENIA", "SOLOMON ISLANDS", "SOMALIA", "SOUTH AFRICA", "SOUTH GEORGIA AND THE SOUTH SANDWICH ISLANDS", "SPAIN", "SRI LANKA", "SUDAN", "SURINAME", "SVALBARD AND JAN MAYEN", "SWAZILAND", "SWEDEN", "SWITZERLAND", "SYRIAN ARAB REPUBLIC", "TAIWAN, PROVINCE OF CHINA", "TAJIKISTAN", "TANZANIA, UNITED REPUBLIC OF", "THAILAND", "TIMOR-LESTE", "TOGO", "TOKELAU", "TONGA", "TRINIDAD AND TOBAGO", "TUNISIA", "TURKEY", "TURKMENISTAN", "TURKS AND CAICOS ISLANDS", "TUVALU", "UGANDA", "UKRAINE", "UNITED ARAB EMIRATES", "UNITED KINGDOM", "UNITED STATES", "UNITED STATES MINOR OUTLYING ISLANDS", "URUGUAY", "UZBEKISTAN", "VANUATU", "VENEZUELA", "VIET NAM", "VIRGIN ISLANDS, BRITISH", "VIRGIN ISLANDS, U.S", "WALLIS AND FUTUNA", "WESTERN SAHARA", "YEMEN", "ZAMBIA", "ZIMBABWE"]
     if(listap.indexOf(p)==-1){
         alert("País no válido")
@@ -140,31 +119,38 @@ function checkpais(pais){
     else{
         document.getElementsByName("pais")[0].value= p;
     }
-    cargarciudades(pais);
-}
-
-function cargarciudades(p){
+    cargarciudades(p);
 
 }
 
-///////////////////////////////////////////////////////////////////////
-function mailcolor(mail){
-    mail.style.color="black";
+function cargarciudades(pais){
+    obtainDt(pais);
 }
 
-function mailclear(mail){
-    document.getElementById("mail").value= "";
-}
+ function obtainDt(pais){
+        if (window.XMLHttpRequest) {
+            // codigo para IE7+, Firefox, Chrome, Opera, Safari
+            xmlt = new XMLHttpRequest();
+        }
+        else {
+            // codigo para IE6, IE5
+            xmlt = new ActiveXObject ("Microsoft.XMLHTTP");
+        }
+        xmlt.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
 
-function checkmail(mail){
-    var patron= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(!mail.value.match(patron)){
-        alert("Correo incorrecto");
-        mailclear();
+                    var xmlC = xmlt.responseXML;
+                    var nuevaf="";
+                    var w = xmlC.getElementById(pais);
+                    var y = w.getAttributeNode("id");
+                    var x = w.childNodes;
+                    for(var j=0;j<x.length;j++){
+                        alert(x[j].childNodes[0].nodeValue)
+                        nuevaf=nuevaf+"<option value=\""+x[j].childNodes[0].nodeValue+"\"/>";
+                    }alert(nuevaf);
+                    document.getElementById('citi').innerHTML = nuevaf;
+            }
+        };
+        xmlt.open('GET', 'ciudades.xml', true);
+        xmlt.send(null);
     }
-}
-
-
-///////////////////////////////////////////////////////////////////////////
-
-
--- html/js/tablas.js
+++ html/js/tablas.js
@@ -1,7 +1,6 @@
 
-
 $(document).ready(function(){
-    ciudades= [["Madrid", 23, 30, 115, 225, true, "F0FF00"],
+    ciudads= [["Madrid", 23, 30, 115, 225, true, "F0FF00"],
         ["Barcelona", 10, 80, 38, 193,  false, "191970"],
         ["Valencia", 20, 54, 27, 177, false, "F5FFFA"],
         ["Sevilla", 23, 56, 66, 253, true, "FFE4E1"],
@@ -23,8 +22,141 @@ $(document).ready(function(){
                   //      ["Granada", 19, 10, 95, 192, true, "FFC0CB"],
         ["Elche", 31, 29, 71, 182, false, "DDA0DD"]];
 
-    rellenarL(ciudades);
+    dats=["Hotel", "Temperatura", "Humedad", "Ruido", "Nivel de luz", "Movimiento", "Color luz"];
+ciudades=[];
+    datos=[];
+
+    obtainDt();
+    //alert(toXml(ciudads));
+    //alert("datos1: "+datos)
+   // alert(datos.length);
+    tablap();
     rellenarT(0,"#p1");
+
+
+
+
+    function tablap(){
+        rellenarCAb(datos);
+        rellenarL(ciudades);
+    }
+
+
+    function obtainDt(){
+        if (window.XMLHttpRequest) {
+            // codigo para IE7+, Firefox, Chrome, Opera, Safari
+            xmlt = new XMLHttpRequest();
+        }
+        else {
+            // codigo para IE6, IE5
+            xmlt = new ActiveXObject ("Microsoft.XMLHTTP");
+        }
+        xmlt.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                obtainInf(xmlt);
+               /* var tx=xmlt.responseText;
+                var res=tx.replace(/<\?\w*\?>/g,"");
+                res=tx.replace(/<\//g,"2");
+                res=res.replace(/</g,"1");
+                res=res.replace(/>/g,"3");
+                res=res.replace(/1\w*3/g,"abro");
+                res=res.replace(/2\w*3/g,"cierro");
+                res=res.replace(/\n/g,"");
+                res=res.replace(/\n/g,"");
+                res=res.replace(/\s/g,"");
+                */
+                //var tx=xmlt.responseXML;
+                //var xxx=xmlt.childNodes;
+                //tx = xmlt.documentElement.childNodes;
+                //var xx=xxx.length;alert("dios");
+                //tx=xmlt.responseText;
+                //alert("res="+xx);
+            }
+        };
+        xmlt.open('GET', 'tablas.xml', true);
+        xmlt.send(null);
+    }
+
+    function obtainInf(st){
+        //alert("cambio de estatus:"+st);
+
+        var y=st.responseXML;
+        //var zz= z.getElementsByTagName("Habitacion");
+        //alert(zz);
+        var w = y.documentElement.childNodes;
+
+
+        //var w = xmlDoc.documentElement.childNodes;
+        //aux="";
+        //var w = y.documentElement.childNodes;
+        var w = y.childNodes;
+        //alert("w.lent="+w.length);
+        //alert("w[0].name: "+w[0].nodeName);
+        //alert("w[0].childondes.name:"+w[0].childNodes[0].nodeName)
+
+        var aux2= new Array (w.length);
+        for(var h=0;h<w.length;h++){
+            var x=w[h].childNodes;
+            //alert("x.lent="+x.length)
+            //alert("x.name: "+x.nodeName)
+            //alert("x[0].name: "+x[0].nodeName)
+            //alert("x.value: "+x.nodeValue);
+            //alert("x[0].value: "+x[0].nodeValue);
+            var aux1= [];
+            for (var i = 0; i < x.length ;i++) {
+              var z=x[i].childNodes;
+              //alert("z.length: "+z.length);
+              //alert("z.name: "+x[i].nodeName);
+              //aux=aux+x[i].nodeName;
+              //aux.push(x[i].nodeName);
+                var aux=[];
+
+                for(var k=0;k<z.length;k++){
+                    var a=z[k].childNodes;
+                    //alert("a.length"+a.length);
+                    //alert("z.Name: "+z[k].nodeName);
+                  //  alert("a[k].value: "+a[0].nodeValue);
+                    aux.push(a[0].nodeValue)
+                    if(i==0){
+                        aux1.push(z[k].nodeName);
+                        //alert("z.Name: "+z[k].nodeName)
+                    }
+                }
+              //aux=aux+z[0].nodeValue+"\n";
+              //aux.push(z[0].nodeValue);
+                aux2[i]=aux;
+                //alert("aux["+i+"]: "+aux2[i]);
+                //txt += x[i].nodeName + ": " + 		 x[i].childNodes[0].nodeValue + "<br>";
+          }
+
+        }
+
+
+       // var y=z.documentElement.childNodes;
+        //alert("1 "+y.length);
+
+        //for(var i=0;x.length;i++){
+            //zz[i].getElementsByTagName[0];
+           // alert("ZZ="+ zz[0].getElementsByTagName("Hotel")[0].childNodes[0].nodeValue);
+            //alert("ZZ="+ zz[cambio de ciudad].getElementsByTagName("Hotel")[tiene que ser 0 porque solo hay uno].childNodes[0].nodeValue);
+        //}
+        ciudades=aux2;
+        datos=aux1;
+       // alert("datos: "+datos)
+    };
+
+    function toXml(arr){
+        var sal="<Habitaciones>";
+        for(var i=0;i<arr.length;i++){
+            sal=sal+"<Habitacion id=\""+i+"\">";
+            for (var j=0;j<arr[i].length;j++){
+                sal=sal+"<"+datos[j]+">"+arr[i][j]+"</"+datos[j]+">";
+            }
+            sal=sal+"</Habitacion>";
+        }
+        sal=sal+"</Habitaciones>";
+        return sal;
+    };
 
 
     $("th").filter(".lk").click(function(){
@@ -66,6 +198,19 @@ $(document).ready(function(){
         }
     });
 
+    function rellenarCAb(dat){//alert("aki");
+        var a = dat[0];
+        var dat=datos;
+        var nuevaf ="<tr>";
+        for (var i=0;i<dat.length;i++){
+            nuevaf=nuevaf+"<th>"+dat[i]+"</th>";
+        }
+        nuevaf=nuevaf+"</tr>";
+        $("#tablad").append(nuevaf);
+    };
+
+
+
     function rellenarL(cad){
         var num=cad.length;
         num=num/5;
@@ -90,7 +235,7 @@ $(document).ready(function(){
             n=addfila(numeroColumnas, i);
             $("#tablad").append(n);
         }
-    }
+    };
 
     function addfila(nCol, indice){
         var nuevafila ="<tr";
